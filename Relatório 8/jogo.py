d0b493d4f015575888544bf15aef5989629ba712
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2024-05-07 16:03:51 -0300

    Updated Relatório 8

--- Relatório 8/jogo.py
+++ Relatório 8/jogo.py
@@ -6,28 +6,50 @@ class Jogo:
 
     def create_jogador(self, nome: str, id: int):
         query = "CREATE (jogador:Jogador {nome: $nome, id: $id})"
-        self.database.execute_query(query, {"nome": nome}, {"id": id})
+        parameters = {"nome": nome, "id": id}
+        self.database.execute_query(query, parameters)
 
     def delete_jogador(self, id: int):
         query = "MATCH (jogador:Jogador {id: $id}) DETACH DELETE jogador"
-        self.database.execute_query(query, {"id": id})
+        parameters = {"id": id}
+        self.database.execute_query(query, parameters)
 
     def update_jogador(self, nome: str, id: int):
         query = "MATCH (jogador:Jogador {id: $id}) SET jogador.nome = $nome"
-        self.database.execute_query(query, {"nome": nome}, {"id": id})
+        parameters = {"nome": nome, "id": id}
+        self.database.execute_query(query, parameters)
 
     def get_jogador(self, id: int):
         query = "MATCH (jogador:Jogador {id: $id}) RETURN jogador"
-        return self.database.execute_query(query, {"id": id})
+        parameters = {"id": id}
+        return self.database.execute_query(query, parameters)
     
     def create_partida(self, id: int):
         query = "CREATE (partida:Partida {id: $id})"
-        self.database.execute_query(query, {"id": id})
+        parameters = {"id": id}
+        self.database.execute_query(query, parameters)
 
     def delete_partida(self, id: int):
         query = "MATCH (partida:Partida {id: $id}) DETACH DELETE partida"
-        self.database.execute_query(query, {"id": id})
+        parameters = {"id": id}
+        self.database.execute_query(query, parameters)
 
     def update_partida(self, id: int):
         query = "MATCH (partida:Partida {id: $id}) SET partida.id = $id"
-        self.database.execute_query(query, {"id": id})
+        parameters = {"id": id}
+        self.database.execute_query(query, parameters)
+
+    def get_partida_winner(self, id: int):
+        query = "MATCH (jogador:Jogador)-[r:JOGOU_EM {vitória: true}]->(partida:Partida {id: $id}) RETURN jogador"
+        parameters = {"id": id}
+        return self.database.execute_query(query, parameters)
+    
+    def get_jogador_matches(self, id: int):
+        query = "MATCH (jogador:Jogador {id: $id})-[r:JOGOU_EM]->(partida:Partida) RETURN partida"
+        parameters = {"id": id}
+        return self.database.execute_query(query, parameters)
+    
+    def create_relacionamento_jogador_partida(self, id_jogador: int, id_partida: int, vitória: bool):
+        query = "MATCH (jogador:Jogador {id: $id_jogador}), (partida:Partida {id: $id_partida}) CREATE (jogador)-[r:JOGOU_EM {vitória: $vitória}]->(partida)"
+        parameters = {"id_jogador": id_jogador, "id_partida": id_partida, "vitória": vitória}
+        self.database.execute_query(query, parameters)
--- Relatório 8/main.py
+++ Relatório 8/main.py
@@ -1,9 +1,46 @@
 from database import Database
+from jogo import Jogo
 
 # cria uma instância da classe Database, passando os dados de conexão com o banco de dados Neo4j
 db = Database("bolt://44.204.86.97:7687", "neo4j", "ejection-precision-fort")
-# db.drop_all()
+db.drop_all()
 
+# cria uma instância da classe Jogo, passando a instância da classe Database
+jogo = Jogo(db)
+
+# cria 10 jogadores
+for i in range(10):
+    jogo.create_jogador(f"Jogador {i}", i)
+
+# cria 5 partidas
+for i in range(5):
+    jogo.create_partida(i)
+
+# cria relacionamentos entre jogadores e partidas (cada partida com 2 jogadores)
+
+# jogadores 0, 1, 2, 3 e 4 venceram as partidas 0, 1, 2, 3 e 4, respectivamente
+for i in range(5):
+    jogo.create_relacionamento_jogador_partida(i, i, True)
+
+# jogadores 5, 6, 7, 8 e 9 perderam as partidas 0, 1, 2, 3 e 4, respectivamente
+for i in range(5, 10):
+    jogo.create_relacionamento_jogador_partida(i, i - 5, False)
+
+# recupera o jogadore que venceu cada partida
+for i in range(5):
+    print(f"Jogador que venceu a partida: {i}")
+    resultado = jogo.get_partida_winner(i)
+    if resultado:
+        print(resultado[0]["jogador"]["nome"])
+    else:
+        print("Partida não finalizada")
+
+# recupera as partidas de cada jogador
+for i in range(10):
+    print(f"Partidas do jogador: {i}")
+    resultado = jogo.get_jogador_matches(i)
+    for record in resultado:
+        print(record["partida"]["id"])
 
 
 
